e05d35e1bdf9fb522f4449cb625bf5f913a1d75a
Lorenzo Carrer <lorenzo.020104@bento.local>
2021-12-07 11:35:54 -0300

    Cadastro de Usuario

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -4,10 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable;
 
-class Usuario extends Model
-{
+class Usuario extends Model implements Authenticatable{
+
+    protected $hidden = ['password'];
+
     use HasFactory;
 
     public $timestamps = false;
-}
+
+    public function getAuthIdentifierName(){
+        return 'id';
+    }
+
+    public function getAuthIdentifier(){
+        return $this->id;
+    }
+
+    public function getAuthPassword(){
+        return $this->password;
+    }
+
+    public function getRememberToken(){
+
+    }
+
+    public function setRememberToken($value){
+
+    }
+
+    public function getRememberTokenName(){
+
+    }
+};
